92ab5a3414a8f78dfa4dc8a4f3f56b7c6d1ff0da
partyrico <cjmoynihan@gmail.com>
2018-01-13 16:19:10 -0500

    The database querying has been fully fleshed out. Next will work on web capabilities.

--- icebreaker-backend/database.py
+++ icebreaker-backend/database.py
@@ -11,17 +11,16 @@ import sqlite3 as sql
 from datetime import datetime
 
 database_name = "server_info.db"
+def log_mistake(error):
+    """If something goes wrong, ignore it (don't crash) and save the error"""
+    with open('ignores.log', 'a') as f:
+        f.write("Had conflict at {time}.\nError was {error}\n".format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), error=str(error)))
 
 class Database:
     def __init__(self):
         self.conn = sql.connect(database_name)
         self.c = self.conn.cursor()
         self.create_tables()
-
-    def log_mistake(self, error):
-        """If something goes wrong, ignore it (don't crash) and save the error"""
-        with open('ignores.log', 'a') as f:
-            f.write("Had conflict at {time}.\nError was {error}\n".format(time=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), error=str(error)))
 
     def create_tables(self):
         """
@@ -32,17 +31,17 @@ class Database:
             name text not null
         )""")
         self.c.execute("""CREATE TABLE IF NOT EXISTS rooms_info(
-            room_password text primary key check(length(room_password) == 4), room_name text not null,
+            room_id integer primary key autoincrement, room_name text not null,
             room_description text,
             expire_date datetime not null,
             dating boolean not null check(dating in (0,1))
         )""")
         self.c.execute("""CREATE TABLE IF NOT EXISTS room_users(
             id integer primary key autoincrement,
-            room_password text,
+            room_id integer,
             user_id integer
         )""")
-        self.c.execute("""CREATE UNIQUE INDEX IF NOT EXISTS room_user ON room_users(room_password, user_id)""")
+        self.c.execute("""CREATE UNIQUE INDEX IF NOT EXISTS room_user ON room_users(room_id, user_id)""")
         self.c.execute("""CREATE TABLE IF NOT EXISTS user_chats(
             chat_id integer primary key autoincrement,
             user1_id integer not null,
@@ -50,6 +49,18 @@ class Database:
             CONSTRAINT user_order check(user1_id<user2_id)
         )""")
         self.c.execute("""CREATE UNIQUE INDEX IF NOT EXISTS user_chat ON user_chats(user1_id, user2_id)""")
+        self.c.execute("""CREATE TABLE IF NOT EXISTS passphrases(
+            room_id integer primary key,
+            room_passphrase text not null
+            )""")
+        self.conn.commit()
+
+    def _generate_passphrases(self):
+        passphrase = lambda n: ''.join(chr(n//(26**i)%26+97) for i in range(4))
+        l = [passphrase(i) for i in range(26**4)]
+        from random import shuffle
+        shuffle(l)
+        self.c.executemany("INSERT INTO passphrases(room_id, room_passphrase) VALUES(?, ?)", (zip(range(26**4), l)))
         self.conn.commit()
 
     def create_user(self, user_name, photo):
@@ -62,22 +73,69 @@ class Database:
             self.c.execute("""INSERT INTO user_info(name) values(?)""", (user_name,))
             user_id = self.c.lastrowid
             # Also add photo
-            with open('user_photos/{0}'.format(user_id), 'rb') as f:
+            with open('user_photos/{0}'.format(user_id), 'wb') as f:
                 # Photo is baseencoded, we don't need to decode it though
                 f.write(photo)
             self.conn.commit()
             return user_id
         except sql.IntegrityError as e:
             # insert failed (probably user_name is null)
-            self.log_mistake(e)
+            log_mistake(e)
             return None
 
-    def get_rooms(self, user_id):
+    def _get_rooms(self, user_id):
         """
         Gets all the rooms that a user is in
         """
-        self.c.execute("SELECT room_password WHERE user_id = ?", (user_id,))
+        self.c.execute("SELECT room_id FROM room_users WHERE user_id = ?", (user_id,))
         return self.c.fetchall()
+
+    def _number_users(self, room_id):
+        """
+        Get the number of users in a room
+        """
+        self.c.execute("SELECT * FROM room_users WHERE room_id = ?", (room_id,))
+        return sum(1 for _ in self.c)
+
+    def _get_room_info(self, room_id):
+        """
+        For a given room_id, gets the name, passphrase, description, and current number of users
+        """
+        self.c.execute("""SELECT (room_name, room_passphrase, room_description)
+            FROM rooms_info INNER JOIN passphrases
+                ON rooms_info.room_id = passphrases.room_id
+            WHERE room_id = ?""", (room_id,))
+        return {k: v for (k, v) in zip(
+            ('name', 'passphrase', 'description', 'num_users'),
+            list(self.c.fetchone()) + [self._number_users(room_id)]
+        )}
+
+    def available_rooms(self, user_id):
+        """
+        Gets the room_id, name, passphrase, description and number of users for each room a user has
+        """
+        return [self._get_room_info(room_id) for room_id in self._get_rooms(user_id)]
+
+    def get_photo(self, user_id):
+        """
+        Returns the base64 encoding of a user's profile image
+        """
+        with open('user_photos/{0}.png'.format(user_id), 'rb') as f:
+            read_data = f.read()
+        return read_data
+
+    def room_users(self, room_passphrase, request_user_id=None):
+        """
+        Gets all the users from the room, and returns a list of their information
+        (excluding the user that requested the data)
+        """
+        self.c.execute("""SELECT (user_id, name) from room_users
+            INNER JOIN user_info ON room_users.user_id = user_info.user_id
+            INNER JOIN passphrases ON room_users.room_id = passphrases.room_id
+            WHERE room_passphrase = ?
+        """, (room_passphrase,))
+        return [(user_id, user_name, self.get_photo(user_id))
+                for (user_id, user_name) in self.c if user_id != request_user_id]
 
 if __name__ == '__main__':
     db = Database()
