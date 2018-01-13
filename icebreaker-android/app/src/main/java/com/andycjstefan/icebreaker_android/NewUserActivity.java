72896192c38860399e53410fb8d864d5185f7a6a
unknown <stefan.kussmaul@gmail.com>
2018-01-12 22:10:32 -0500

    New Android Project

---
+++ icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/NewUserActivity.java
@@ -0,0 +1,13 @@
+package com.andycjstefan.icebreaker_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewUserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_user_layout);
+    }
+}
