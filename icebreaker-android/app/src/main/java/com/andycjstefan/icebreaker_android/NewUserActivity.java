806f1345abe177fcdd52d63163e80c58ce13d8d2
unknown <stefan.kussmaul@gmail.com>
2018-01-13 16:39:05 -0500

    Example HTTP Request and New User Keyboard, Error Handling Code

--- icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/NewUserActivity.java
+++ icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/NewUserActivity.java
@@ -5,9 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class NewUserActivity extends Activity {
 
@@ -16,16 +24,15 @@ public class NewUserActivity extends Activity {
     public static final String USER_ID_KEY = "USER_ID_KEY";
 
     private Button nextButton;
-    private String userNameString;
+    private EditText userNameEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_user_layout);
 
-        //Saves the username
-        EditText userNameTextField = (EditText)findViewById(R.id.user_name);
-        userNameString = userNameTextField.getText().toString();
+        userNameEntry = findViewById(R.id.user_name_entry);
+
         //next button(goes to picture upload/taking)
         nextButton = (Button) findViewById(R.id.next_button);
 
@@ -34,12 +41,38 @@ public class NewUserActivity extends Activity {
         if (data.contains(USER_ID_KEY)) {
             onUserIdValidated(data.getInt(USER_ID_KEY, -1));
         }
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://partrico.pythonanywhere.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("NewUser", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("NewUser", "couldn't get request from server");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     // called when user clicks button to submit Name
     public void onNameSubmitted(View view) {
-        // send user to dashboard
-        startActivity(new Intent(this, PictureChoiceActivity.class));
+        // no name entered: show error message
+        if (userNameEntry.getText().toString().isEmpty()) {
+            findViewById(R.id.error_message_text).setVisibility(View.VISIBLE);
+        } else {
+            // send user to dashboard
+            startActivity(new Intent(this, PictureChoiceActivity.class));
+        }
     }
 
     // user id validated without setting up new profile: launch dashboard immediately
