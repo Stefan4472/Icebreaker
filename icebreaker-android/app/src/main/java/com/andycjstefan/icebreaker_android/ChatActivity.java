ae95976bd8cc2df8657427e5817ed4b435e89d53
unknown <stefan.kussmaul@gmail.com>
2018-01-13 09:22:12 -0500

    ChatActivity Taking Intent Extras to Specify User and Partner Ids

--- icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/ChatActivity.java
+++ icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/ChatActivity.java
@@ -11,7 +11,14 @@ import android.widget.EditText;
 import java.util.LinkedList;
 import java.util.List;
 
+// The ChatActivity displays and facilitates a private conversation between two given userIds--
+// the user on this phone (specified by userId) and the user being messaged (partnerId). Those
+// are used to determine the coloring and position of chat messages. TODO: NETWORK CALLS
 public class ChatActivity extends Activity {
+
+    // keys used to send "extras" to ChatActivity via the calling Inent
+    public static final String EXTRA_USER_ID = "EXTRA_USER_ID";
+    public static final String EXTRA_PARTNER_ID = "EXTRA_PARTNER_ID";
 
     private static String[] messageObjects = {
             "1,2,Hey how are you?,1515814620397",
@@ -31,12 +38,19 @@ public class ChatActivity extends Activity {
     private List<Message> messages = new LinkedList<>();
 
     // id of this user, and the user chatting with TODO: SEND IN BUNDLE
-    private int userId = 1, partnerId = 2;
+    private int userId = -1, partnerId = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_layout);
+
+        userId = getIntent().getIntExtra(EXTRA_USER_ID, -1);
+        partnerId = getIntent().getIntExtra(EXTRA_PARTNER_ID, -1);
+
+        if (userId == -1 || partnerId == -1) {
+            throw new IllegalArgumentException("Missing UserId or PartnerId Extra! Undefined");
+        }
 
         chatRecyclerView = (RecyclerView) findViewById(R.id.chat_recycler_view);
         chatRecyclerView.setLayoutManager(new LinearLayoutManager(this));
--- icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/DashboardActivity.java
+++ icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/DashboardActivity.java
@@ -26,6 +26,9 @@ public class DashboardActivity extends Activity {
 
     // responds to user clicking to see chats they are in
     public void onViewChatsClicked(View view) {
-        startActivity(new Intent(this, ChatActivity.class));
+        Intent chat_intent = new Intent(this, ChatActivity.class);
+        chat_intent.putExtra(ChatActivity.EXTRA_USER_ID, 1); // TODO: PUT CURRENT USER ID
+        chat_intent.putExtra(ChatActivity.EXTRA_PARTNER_ID, 2); // TODO: PUT PARTNER ID
+        startActivity(chat_intent);
     }
 }
