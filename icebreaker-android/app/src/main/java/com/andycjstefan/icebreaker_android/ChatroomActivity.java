c2f0b49f43110e485a3ed365bc902a459c2892b2
unknown <stefan.kussmaul@gmail.com>
2018-01-12 22:18:06 -0500

    Added Placeholder Activities

---
+++ icebreaker-android/app/src/main/java/com/andycjstefan/icebreaker_android/ChatroomActivity.java
@@ -0,0 +1,13 @@
+package com.andycjstefan.icebreaker_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatroomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chatroom_layout);
+    }
+}
